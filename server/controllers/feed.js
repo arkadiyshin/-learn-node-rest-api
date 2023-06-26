2388f9f64a3e409163b0966b5ea8da47b7fe09a8
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-26 21:05:38 +0200

    feat(server): add POST /feed/post endpoint

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,11 +1,27 @@
 
 exports.getPosts = (req, res, next) => {
-  return res.status(200).json({
+  res.status(200).json({
     message: 'Success!',
     posts: [{
       _id: 1,
       title: 'First post',
-      content: '',
+      content: 'This is  the first post',
+      imageUrl: 'images/book.jpg',
+      creator: { name: 'Arkadii' },
+      createdAt: new Date()
+    }]
+  })
+}
+
+exports.createPosts = (req, res, next) => {
+  const { title, content } = req.body;
+
+  res.status(201).json({
+    message: 'Post created successfuly!',
+    posts: [{
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
       creator: { name: 'Arkadii' },
       createdAt: new Date()
     }]
