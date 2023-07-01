0709cb3a9d18ddb1e83ee630c1b8a6401728bb0d
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-07-01 19:58:55 +0200

    feat(server): add user model

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/user');
 
 require('dotenv').config();
 
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.statusCode;
---
+++ server/routes/user.js
@@ -0,0 +1,8 @@
+const userController = require('../controllers/user')
+
+const express = require('express');
+const { body } = require('express-validator');
+
+const router = express.Router();
+
+router.put('/signup',);
