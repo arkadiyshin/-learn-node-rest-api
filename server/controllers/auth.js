3170b97c8fccbf4b164a0a314455ca7ceb078816
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-07-03 19:39:23 +0200

    feat(server): create jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user');
 
@@ -56,7 +57,13 @@ exports.login = (req, res, next) => {
         error.statusCode = 401;
         throw error
       }
-
+      const token = jwt.sign({
+        email: loadedUser.email,
+        userId: loadedUser._id.toString()
+      },
+        'secret',
+        { expiresIn: '1h' });
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() })
     })
     .catch(err => {
       if (!err.statusCode) {
