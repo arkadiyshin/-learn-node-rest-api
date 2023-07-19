a70ace5000076684b14303ac33e667bb75d4643f
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-07-19 23:12:28 +0200

    feat(client): send create post and get posts queries

--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -54,27 +54,47 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch(`http://localhost:8080/feed/posts?page=${page}`, {
-      headers: {
-        Authorization: `Bearer ${this.props.token}`
-      }
-    }
-    )
-      .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
+    const graphqlQuery = {
+      query: `
+      {
+        posts {
+          posts {
+            _id
+            title
+            content
+            creator {
+              name
+            }
+            createdAt
+          }
         }
+      }
+      `
+    }
+
+    fetch(`http://localhost:8080/graphql`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(graphqlQuery)
+    })
+      .then(res => {
         return res.json();
       })
       .then(resData => {
+        if (resData.errors) {
+          throw new Error('Fetching posts failed.');
+        }
         this.setState({
-          posts: resData.posts.map(post => {
+          posts: resData.data.posts.posts.map(post => {
             return {
               ...post,
               imagePath: post.imageUrl
             }
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.posts.totalItems,
           postsLoading: false
         });
       })
@@ -158,7 +178,6 @@ class Feed extends Component {
       `
     }
 
-    console.log(graphqlQuery)
     fetch(`http://localhost:8080/graphql`, {
       method: 'POST',
       headers: {
@@ -181,15 +200,25 @@ class Feed extends Component {
         if (resData.errors) {
           throw new Error('Post adding failed.');
         }
-        // const post = {
-        //   _id: resData.post._id,
-        //   title: resData.post.title,
-        //   content: resData.post.content,
-        //   creator: resData.post.creator,
-        //   createdAt: resData.post.createdAt
-        // };
+        const post = {
+          _id: resData.data.createPost._id,
+          title: resData.data.createPost.title,
+          content: resData.data.createPost.content,
+          creator: resData.data.createPost.creator,
+          createdAt: resData.data.createPost.createdAt
+        };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            )
+            updatedPosts[postIndex] = post
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
